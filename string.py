7c53636703e5017a55286bfe4339beaf32bf8851
G.Venkata sasi kumar <sasiyadav667@gmail.com>
2025-11-05 18:30:58 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,26 @@
+print("hello world")
+
+#STRING METHODS
+#lower
+#upper
+#count
+#find
+
+
+
+#LOWER
+python="PYTHON PROGRAMING"
+print(python.lower())
+
+
+python="python programing"
+print(python.upper())
+
+python="python programing"
+print(python.count('p'))
+
+python="python programing"
+print(python.find('m'))
+
+
+
